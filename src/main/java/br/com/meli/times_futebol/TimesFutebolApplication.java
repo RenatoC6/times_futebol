f0e4a2af6e34ff30e55d5eec8349276fc7e90e49
Renato Campion <campionr6@gmail.com>
2025-06-27 14:51:45 -0300

    Initial commit

---
+++ src/main/java/br/com/meli/times_futebol/TimesFutebolApplication.java
@@ -0,0 +1,13 @@
+package br.com.meli.times_futebol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimesFutebolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimesFutebolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/meli/times_futebol/TimesFutebolApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meli.times_futebol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimesFutebolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
